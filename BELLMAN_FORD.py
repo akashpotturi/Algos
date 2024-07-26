486aca6c3fa141974022dcd97fd39cda321f1473
akashpotturi <139764701+akashpotturi@users.noreply.github.com>
2024-07-26 12:26:33 +0530

    Update BELLMAN_FORD.py

--- BELLMAN_FORD.py
+++ BELLMAN_FORD.py
@@ -1,4 +1,5 @@
 def bellman_ford(self, V, edges, S):
+        #can be used in directed graphs and can be used with negative weights where dijktras fail with negative weights
         dis = [10**8]*V
         dis[S] = 0
         for i in range(V-1):
